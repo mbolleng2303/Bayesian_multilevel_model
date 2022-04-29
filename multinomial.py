cffe6c096320a3638f458d38cb3d030a031c81c1
mbolleng2303 <93711211+mbolleng2303@users.noreply.github.com>
2022-04-29 17:04:47 +0200

    Add files via upload

---
+++ multinomial.py
@@ -0,0 +1,102 @@
+import warnings
+from pathlib import Path
+import pickle
+from collections import OrderedDict
+import pandas as pd
+import numpy as np
+from scipy import stats
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import (roc_curve, roc_auc_score, confusion_matrix, accuracy_score, f1_score,
+                             precision_recall_curve)
+import theano.tensor as tt
+#from mlxtend.plotting import plot_confusion_matrix
+import theano
+import pymc3 as pm
+from pymc3.variational.callbacks import CheckParametersConvergence
+import statsmodels.formula.api as smf
+import arviz as az
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import seaborn as sns
+from IPython.display import HTML
+warnings.filterwarnings('ignore')
+np.random.seed(0)
+N_sample = 1000
+# patient id
+patient_id = np.linspace(0, N_sample, N_sample).astype(int)
+# age
+mu = 50
+sigma = 5
+age = np.random.normal(mu, sigma, N_sample).astype(int)
+plt.hist(age, bins='auto')
+# plt.show()
+# gender
+gender = np.random.choice([0, 1, 3, 4], N_sample, p=[0.46, 0.46, 0.04, 0.04])
+plt.hist(gender, bins='auto')
+# plt.show()
+
+# Smoking
+smoking = np.random.choice([0, 1, 2, 8], N_sample, p=[0.3, 0.3, 0.3, 0.1])
+plt.hist(smoking, bins='auto')
+# plt.show()
+
+# Fever
+fever = np.random.choice([0, 1], N_sample, p=[0.5, 0.5])
+plt.hist(fever, bins='auto')
+# plt.show()
+
+# Vomiting
+vomiting = np.random.choice([0, 1], N_sample, p=[0.5, 0.5])
+plt.hist(vomiting, bins='auto')
+# plt.show()
+
+# Severity
+outcome = np.random.choice([0, 1, 2], N_sample, p=[0.6, 0.25, 0.15])  #np.random.choice([0, 1], N_sample, p=[0.75, 0.25])#
+plt.hist(outcome, bins='auto')
+# plt.show()
+
+data = list(zip(age, gender, smoking, fever, vomiting, outcome))
+
+df = pd.DataFrame(data,
+                  columns=['age', 'gender', 'smoking', 'fever', 'vomiting', 'outcome'])
+df.to_csv('data.csv', index=False)
+
+warnings.filterwarnings('ignore')
+np.random.seed(0)
+
+data = pd.read_csv('data.csv')
+data.info()
+data.head()
+X = data.drop('outcome', axis=1)
+y = data.outcome
+# models
+simple_model = "outcome ~ age + gender + smoking"
+full_model = "outcome ~ age + gender + smoking + fever + vomiting"
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    test_size=0.2,
+                                                    random_state=42)
+labels = X_train.columns
+X_shared = theano.shared(X_train.values)
+
+X_train = np.array(X_train)
+nbr_variable = X_train.shape[1]
+with pm.Model() as multinomial:
+    a = pm.Normal("a", mu=0, sigma=1, shape=3)  # intercepts
+    b = pm.Normal("b", mu=0, sigma=1.5, shape=3)  # association of income with choice
+
+    s0 = a[0] + b[0] * X_shared[:, 0]
+    s1 = a[1] + b[1] * X_shared[:, 1]
+    s2 = np.zeros(800)#a[2] + b[2] * X_train[:, 2]  # pivoting the intercept for the third category
+    s = pm.math.stack([s0, s1, s2]).T
+
+    p_ = tt.nnet.softmax(s)
+    career_obs = pm.Categorical("career", p=p_, observed=y_train)
+
+    trace_multinomial = pm.sample(draws=1000,
+                           tune=100,
+                           chains=1,
+                           cores=1,
+                           init='adapt_diag')
+az.summary(trace_multinomial, round_to=2)
+
